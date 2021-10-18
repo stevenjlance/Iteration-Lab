6ab200c39280a7bf258106afee62d4bbbb7a8743
StevenLance <44912347+stevenjlance@users.noreply.github.com>
2021-10-18 12:09:31 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Survey Says - JavaScript Iteration Lab
+
+![Replit](example.png)
+
+## The Goal
+
+The information in the database file attached here has results from a survey Jeff took of some of his friends. He's going to use that information to learn some things about the friends who answered the survey. For example:
+* What percent of his friends are programmers?
+* Do they like similar or dissimilar music?
+* How many of them live in each state?
+
+One warning: these are real people, and real people don't always provide the easiest data to work with. Get ready for typos, unexpected blanks, and outright silliness (Max, for example, isn't 100 years old).
+
+In the `index.js` file, Jeff has written some questions he has. Use your knowledge of for loops, and the `.map()` array method to complete these challenge!
+
+## Getting Started
+
+If you're using an environment like Glitch that focuses on HTML, be sure to open index.html in a web browser and then also open the JavaScript console and confirm that you see "Script Running".
+
+If you're using an environment like Repl.it that focuses on scripting languages, you should be able to push "play" or "run" and see the "Script Running" confirmation in the terminal.
+
+Once you've confirmed that the script runs, open `index.js` and get started on the challenges.
+
+## The Lab
+
+The challenges of this lab are documented directly in the `index.js` as comments.
+
+## Extensions
+
+Exercises 9 and 10 are more complicated than most of the prior ones, and will likely require you to do some additional research and trial and error. 
---
+++ index.js
@@ -0,0 +1,88 @@
+console.log("Script running!")
+
+// This is the arrays you'll interact with! It's pretty good. Scroll down for the first challenge.
+let names = ["Daniel","Yaika","Luke","William","Kathryn","Taylor","Makenzie","Brenton","Max","Mike","Olivia","Rebecca","Daley","Tim","Ahmad","Chelsey","Faedra","Terrace","Kate","Zaire"]
+
+let nickname = ["Danny","Yikes","Loki","Wild Bill","Kath","Tay","Kenzy","Smelly","Smallman","","Liv","Becca","","Tim","Ahms","","Mattox","T-Dawg","KMoMo",""]
+
+let age = [31,30,28,29,51,25,27,30,100,19,26,29,25,27,29,29,43,31,27,29]
+
+let favoriteBand = ["Booker T and the MGs","OAR","Miike Snow","Pentatonix","Imagine Dragons","Joseph","Something Corporate","Guster","adele","Led Zeppelin","","The Beatles","Leon Bridges","Kishi Bashi","Saves The Day","Eli young band","OAR","Chicago","Adele","MCR"]
+
+let favoriteShow = ["Mad Men","Penny Dreadful","Game of Thrones","Steven Universe","Game of Thrones","The West Wing","Bob's Burgers","Rick and Morty","Game of Thrones","Welcome to the NHK","Parks and Rec","Hunter x Hunter","Stranger Things","The Wire","The West Wing","WOD","The Office","The Office","Game of Thrones","Rick and morty"]
+
+let isProgrammer  = [true,false,false,false,false,true,false,false,true,false,true,false,false,true,false,false,false,false,false,false]
+
+let state = ["NY","TX","N/A - UK","TX","NC","NY","TX","ON","CA","AZ","AZ","TX","TN","CA","MI","ID","WI","AZ","AZ","TX"]
+
+let hairColor = ["Brown","Black","Dark Blonde","Brown","Brown","Brown","Brown","Blonde","Brown","Blonde","Blonde","Brown","Brown","Black","Black","Blonde","Brown","Brown","Amber ","Black"]
+
+let motto = ["Don't overthink it.","I guess we'll find out. ","People have catchphrases in America?","Being kind is always a good idea.","You can practice being right or you can practice being kind. ","The work always gets done.","Meh","Ohmydog","Get along little doggy","I never die.","The best is yet to come!","The thing is","You can lock your heart up","Swing Away","That's Dope!","Don't tell me","She is clothed in strength and dignity and she laughs without fear of the future","Beep beep beep. ","Do not pity the dead. Pity the living; most of all","I totally got this Motto/0","Don't overthink it.","I guess we'll find out. ","People have catchphrases in America?","Being kind is always a good idea.","You can practice being right or you can practice being kind. ","The work always gets done.","Meh","Ohmydog","Get along little doggy","I never die.","The best is yet to come!","The thing is","You can lock your heart up","Swing Away","That's Dope!","Don't tell me","She is clothed in strength and dignity and she laughs without fear of the future","Beep beep beep. ","Do not pity the dead. Pity the living; most of all","I totally got this "]
+
+// 1a. console.log the name of every person who responded to the survey
+
+
+// 1b. console.log "Hi, my name is _____, and I live in ____." for every person who responded to the survey
+
+
+// 2a. Create an empty array and name it shortBios. Inside of shortBios, create an object that stores each respondents name, nickname, and age. Then, console.log shortBios.
+
+
+
+// 2b. Add the favorite show and favorite band to each person object inside of the shortBios array. Then, console.log shortBios.
+
+
+
+// 2c. Create a new array called longBios. Inside of longBios, create an object that stores each respondents name, nickname, age, favorite show, favorite band, if they are a programmer, the state they live in, their hair color, and their personal bio. Then, console.log longBios.
+
+
+
+
+// 3a. For each person in longBios, console.log a descriptor of where they live.
+// It will look something like this: "____ lives in _____."
+
+
+
+
+// 3b. console.log each person's nickname.
+// BONUS: Write some control flow that determines whether / how to respond if someone didn't provide a nickname.
+
+
+
+
+// 4. Find the average of the respondents' ages. Console.log this average (please use a loop, don't hard code the answer).
+
+
+
+
+// 5. Print out the percentage of respondents who have brown hair. Console.log this percentage (please use a loop, don't hard code the answer).
+
+
+
+
+// 6. Create two new arrays - one for programmers, and one for non programmers.
+//    Sort all the people in the contacts array into these two more specific arrays.
+
+
+
+
+// 7. Create a new array only for people who listed "Game of Thrones" as their one of their favorite shows.
+
+
+
+
+// 8. Console.log a list of all the bands that are liked by at least two people.
+//    Pro-tip: some respondents capitalized band names and other respondents did not, so for example, if Panic at the Disco isn't listed, your code isn't accurately reporting your results.
+
+
+
+// EXTENSIONS //
+// 9. Use the `.map()` method on the names array to create a new array called formattedNames which concatenates each name to an opening and closing `<h1>` tag.
+
+
+
+// 10. Add each person's name to the webpage inside of the div with the id of "names."
+
+
+
+// 11. Create a bio for each person and add it to the webpage inside of the div with the id of "biographies." Please use all the information about each person in this bio. For example: "_____, better know as _____ to friends currently lives in _____. ______'s motto is _______, which they heard on their favorite TV show ________." Be creative in the bio you create.
